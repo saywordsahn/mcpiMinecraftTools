de29ff56c552def0acb5b4871894defcf9af9d7d
TCC Tutors <saywordsahn@gmail.com>
2021-08-17 16:36:44 +0900

    Implemented Copy/Paste

--- copyPaste.py
+++ copyPaste.py
@@ -2,25 +2,53 @@ from mcpi.minecraft import Minecraft
 from mcpi import block
 mc = Minecraft.create("mc2.tokyocodingclub.com")
 
-player = mc.getPlayerEntityId("TCC_10")
+player = mc.getPlayerEntityId("TCC_03")
 
 pos = mc.entity.getTilePos(player)
 
 
-length = 20
-width = 20
-height = 2
+def copy(l, w, h):
+    pos = mc.entity.getTilePos(player)
+
+    blockInfo = []
+
+    for i in range(l):
+        x = []
+        for j in range(h):
+            y = []
+            for k in range(w):
+                block = mc.getBlock(pos.x + i, pos.y + j, pos.z + k)
+                y.append(block)
+            x.append(y)
+        blockInfo.append(x)
+
+    return blockInfo
+
+def paste(blockInfo):
+    pos = mc.entity.getTilePos(player)
+
+    for i in range(len(blockInfo)):
+        for j in range(len(blockInfo[i])):
+            for k in range(len(blockInfo[i][j])):
+                mc.setBlock(pos.x + i, pos.y + j, pos.z + k, blockInfo[i][j][k])
+
 
 blockInfo = []
+user_input = '-1'
 
-# block = mc.getBlock(pos.x, pos.y - 1, pos.z)
-print(block)
+while user_input != '3':
+    print('1. Copy')
+    print('2. Paste')
+    print('3. Exit')
+    print()
+    user_input = input('Enter your selection (1-3): ')
 
-for i in range(length):
-    x = []
-    for j in range(height):
-        y = []
-        for k in range(width):
-            block = mc.getBlock(pos.x, pos.y, pos.z)
-            print(block)
+    if user_input == '1':
+        length = input('Enter length: ')
+        width = input('Enter width: ')
+        height = input('Enter height: ')
+        blockInfo = copy(length, width, height)
+    if user_input == '2':
+        paste(blockInfo)
+
 
