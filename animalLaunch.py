0bded0e10f11f93cbf397dcf7d7378d097d69508
TA Tutors <saywordsahn@gmail.com>
2021-11-12 19:06:20 +0900

    updated

--- animalLaunch.py
+++ animalLaunch.py
@@ -1,12 +1,45 @@
 from mcpi.minecraft import Minecraft
 import time
 from mcpi.minecraft import ProjectileEvent
+from mcpi import block
 
 mc = Minecraft.create("mc2.tokyocodingclub.com")
 
-p1_name = 'TCC_10'
+p1_name = 'TCC_03'
 
 p1_id = mc.getPlayerEntityId(p1_name)
+
+def create_house(x, y, z):
+
+    width = 5
+    height = 3
+    depth = 6
+
+    # Create a hollow shell made of bricks.
+    mc.setBlocks(x, y, z+3, x+width, y+height, z+3+depth, block.BRICK_BLOCK.id)
+    mc.setBlocks(x+1, y, z+4, x+width-1, y+height-1, z+2+depth, block.AIR.id)
+
+    # Set the floor.
+    mc.setBlocks(x-1, y-1, z+2, x+1+width, y-1, z+4+depth, block.COBBLESTONE.id)
+
+    # Add a Door.
+    mc.setBlock(x+1, y, z+3, block.DOOR_WOOD.id, 0)
+    mc.setBlock(x+1, y+1, z+3, block.DOOR_WOOD.id, 8)
+
+    # Add Windows.
+    mc.setBlocks(x+3, y+1, z+3, x+4, y+2, z+3, block.GLASS.id)
+    mc.setBlocks(x+2, y+1, z+3+depth, x+3, y+2, z+3+depth, block.GLASS.id)
+    mc.setBlocks(x, y+1, z+5, x, y+2, z+7, block.GLASS.id)
+    mc.setBlocks(x+width, y+1, z+5, x+width, y+2, z+7, block.GLASS.id)
+
+    # Add a Roof.
+    for i in range(int(width/2) + 1):
+        mc.setBlocks(x+i, y+height+i, z+3, x+i, y+height+i, z+3+depth, block.STAIRS_WOOD.id, 0)
+        mc.setBlocks(x+width-i, y+height+i, z+3, x+width-i, y+height+i, z+3+depth, block.STAIRS_WOOD.id, 1)
+        # Gable ends.
+        if (int(width/2) - i > 0):
+            mc.setBlocks(x+1+i, y+height+i, z+3, x+width-i-1, y+height+i, z+3, block.BRICK_BLOCK.id, 0)
+            mc.setBlocks(x+1+i, y+height+i, z+3+depth, x+width-i-1, y+height+i, z+3+depth, block.BRICK_BLOCK.id, 1)
 
 
 
@@ -17,8 +50,18 @@ while True:
         x = proj.pos.x
         y = proj.pos.y
         z = proj.pos.z
-        mc.spawnEntity(x, y, z, 20)
-        mc.setBlock(x, y, z, 46)
+        # mc.spawnEntity(x, y, z, 20)
+        size = 5
+        # mc.setBlocks(x - size, y - size, z - size, x + size, y + size, z + size, block.LAVA)
+        for i in range(30):
+            for j in range(30):
+                if i % 5 == 0 and j % 5 == 0:
+                    mc.setBlock(x + i, y, z + j, block.TNT)
+                    mc.setBlock(x + i,y+1 ,z + j, block.TORCH_REDSTONE)
+
+        mc.setBlock(x, y, z, block.TNT)
+        mc.setBlock(x, y + 1, z, block.TORCH_REDSTONE)
+        # create_house(x, y, z)
 
 
 
