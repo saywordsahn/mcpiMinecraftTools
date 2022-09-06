9d10cb9b7f6c3b94f6d21e1ee7b958294a70c64a
TCC Tutors <saywordsahn@gmail.com>
2022-09-06 15:36:58 +0900

    Added draw_x and partially complete draw_o

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,3 +7,31 @@ player = mc.getPlayerEntityId("SayWordSAHN")
 pos = mc.entity.getTilePos(player)
 
 white_concrete = 251
+
+def draw_x(x, y, z, blockType):
+    mc.setBlock(x, y, z, blockType)
+    mc.setBlock(x + 1, y, z + 1, blockType)
+    mc.setBlock(x + 2, y, z + 2, blockType)
+    mc.setBlock(x + 3, y, z + 3, blockType)
+    mc.setBlock(x + 4, y, z + 4, blockType)
+    mc.setBlock(x, y, z + 4, blockType)
+    mc.setBlock(x + 1, y, z + 3, blockType)
+    mc.setBlock(x + 3, y, z + 1, blockType)
+    mc.setBlock(x + 4, y, z, blockType)
+
+def draw_o(x, y, z, blockType):
+    mc.setBlock(x, y, z, blockType)
+    mc.setblock(x + 1, y, z, blockType)
+    mc.setblock(x + 2, y, z, blockType)
+    mc.setblock(x + 3, y, z, blockType)
+    mc.setblock(x + 4, y, z, blockType)
+
+    mc.setBlock(x, y, z + 4, blockType)
+    mc.setblock(x + 1, y, z + 4, blockType)
+    mc.setblock(x + 2, y, z + 4, blockType)
+    mc.setblock(x + 3, y, z + 4, blockType)
+    mc.setblock(x + 4, y, z + 4, blockType)
+
+
+
+draw_x(pos.x, pos.y, pos.z, white_concrete)
